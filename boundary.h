e45776226f012f3392ca95073b2e88feb52209c2
Mingang Jin <farocean97@gmail.com>
2014-04-01 23:05:26 -0400

    add comment to boundary.h

--- boundary.h
+++ boundary.h
@@ -1,12 +1,29 @@
-/* ----------------------------------------------------------------------------
-
-  File:          boundary.h
-
-  Written by:   Wangda Zuo
-
-  Task:         Declare the subroutines in boundary.c
-
----------------------------------------------------------------------------- */
+///////////////////////////////////////////////////////////////////////////////
+///
+/// \file   advection.h
+///
+/// \brief  Solver for advection step
+///
+/// \author Mingang Jin, Qingyan Chen
+///         Purdue University
+///         Jin56@purdue.edu, YanChen@purdue.edu
+///         Wangda Zuo
+///         University of Miami
+///         W.Zuo@miami.edu
+///
+/// \date   8/3/2013
+///
+/// This file provides functions that used for the advection step of FFD method.
+/// The advection starts with \c advect(). Then different subroutines are 
+/// called according to the properties of the variables that are sorted by
+/// the location of variables assigned in the control volume. 
+/// Velocities at X, Y and Z directions are locatted
+/// on the surface of the control volume. They are computed using 
+/// subroutines: \c traceback_uvw
+/// Scalar variables are in the center of control volume and they are computed
+/// using \c trace_scalar().
+///
+///////////////////////////////////////////////////////////////////////////////
 
 void set_vel_bnd(PARA_DATA *para, REAL **var,int **BINDEX);
 
